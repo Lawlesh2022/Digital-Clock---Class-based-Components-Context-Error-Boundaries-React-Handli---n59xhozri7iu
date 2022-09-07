10f20f1519f2ff797f995d555a8f73deca669e8f
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-07 13:57:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+const [datetime, setDatetime] = useState("")
+setInterval(()=>{
+    setDatetime(new Date().toLocaleString())
+}, 1000)
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{datetime}</div>
     </div>
   )
 }
